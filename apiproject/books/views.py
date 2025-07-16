c71c4e5430025779b990da033595057118280fb1
Anant Tyagi <ananttyagi359@gmail.com>
2025-07-16 14:31:01 +0530

    API Integration

--- apiproject/books/views.py
+++ apiproject/books/views.py
@@ -1,62 +1,77 @@
 # from django.shortcuts import render
 # from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
-from rest_framework.decorators import api_view, authentication_classes, permission_classes 
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
+# from .models import Book
+# from .serializers import BookSerializers
+# from rest_framework.decorators import api_view, authentication_classes,permission_classes
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import TokenAuthentication
+# from .models import Book
+# from .serializers import BookSerializers
 
 
 # class BookViewset(viewsets.ModelViewSet):
 #     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
+#     serializer_class = BookSerializers
 
-@api_view(['GET','POST','DELETE','PATCH'])
+
+
+
+from rest_framework.decorators import api_view, authentication_classes,permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from .models import Book
+from .serializers import BookSerializer
+
+    
+
+@api_view(['GET','POST','PATCH'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def book_list_create(request):
     if request.method == 'GET':
         books = Book.objects.all()
-        serializer = BookSerializer(books,many=True)
+        serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
     
-    elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
+    elif request.method =='POST':
+        serializer =BookSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status =status.HTTP_400_BAD_REQUEST)
     
-
-
 @api_view(['GET','PUT','DELETE','PATCH'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-
-def book_list_auth(request,pk):
+def book_list_auth(request, pk):
     try:
         book = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
-        return Response({"error":"Book not found"},status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Book not found."}, status =status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = BookSerializer(book)
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
-        serializer = BookSerializer(book,data=request.data)
+        serializer =BookSerializer(book,data = request.data)
         if serializer.is_valid():
-          serializer.save()
-          return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST)
+    
+
     elif request.method == 'DELETE':
         book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
     elif request.method == 'PATCH':
         serializer = BookSerializer(book, data=request.data, partial=True)  # <- Note partial=True
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
